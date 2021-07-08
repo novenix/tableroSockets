12af486233a58d458727dda89b10ec59319a72c9
novenix <novenixn@gmail.com>
2021-07-08 17:05:49 -0500

    commit security

---
+++ src/main/java/co/edu/escuelaing/interactivebalckboardlife/configurator/MvcConfig.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.interactivebalckboardlife.configurator;
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+public class MvcConfig {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
